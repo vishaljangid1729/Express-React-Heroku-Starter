afa0d80f39b580fba267d5f5d3269eb18ecf0a0d
Vishal Jangid <jangidvishal1999@gmail.com>
2019-10-24 12:34:46 +0530

    minor fix

--- index.js
+++ index.js
@@ -14,14 +14,15 @@ app.get('/api', (req, res)=>{
             res.json({user: response.data});
         })
 })
+const PORT = process.env.PORT || 4000;
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static('client/buid'));
+    app.use(express.static('client/build'));
     app.get('*', (req, res)=>{
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     })
 }
 
-const PORT = process.env.PORT || 4000;
+
 app.listen(PORT,()=> console.log(`Listen on port ${PORT}`) );
 
